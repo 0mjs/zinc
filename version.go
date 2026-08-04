500295da0ec07087cf977a2629535ffcffec2540
0mjs <dev@mattjs.me>
2026-08-04 15:35:15 +0100

    chore: bump version to 0.2.1

--- version.go
+++ version.go
@@ -2,7 +2,7 @@ package zinc
 
 // Version is the current version of Zinc.
 // This should be updated for each release.
-const Version = "0.2.0"
+const Version = "0.2.1"
 
 // GetVersion returns the current version of Zinc.
 func GetVersion() string {
